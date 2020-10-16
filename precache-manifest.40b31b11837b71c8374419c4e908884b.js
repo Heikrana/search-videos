2390923d7925b3b9bb8b1aab555cf3a0c3695caf
vsoni101 <vs201100@gmail.com>
2020-10-16 14:30:22 +0530

    Updates

---
+++ precache-manifest.40b31b11837b71c8374419c4e908884b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4bc40f06be5ecec7599925f659b0d0",
+    "url": "/search-videos/index.html"
+  },
+  {
+    "revision": "d870e754872bcd09ec6a",
+    "url": "/search-videos/static/css/main.0aca868d.chunk.css"
+  },
+  {
+    "revision": "73bb1546f0af120d4293",
+    "url": "/search-videos/static/js/2.a2bfefc6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/search-videos/static/js/2.a2bfefc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d870e754872bcd09ec6a",
+    "url": "/search-videos/static/js/main.7e205cfd.chunk.js"
+  },
+  {
+    "revision": "4aa96a5da06507f93c19",
+    "url": "/search-videos/static/js/runtime-main.42349016.js"
+  }
+]);
